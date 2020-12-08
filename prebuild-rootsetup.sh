0d959bd952f0eec843670efeefc2e6ccfd335ec4
Damien Harwin <damien.harwin@mayden.co.uk>
2020-12-08 17:35:09 +0000

    Fix shell script typo
    Fix shell script typo when checking ~/.bashrc for presence of python27 string

--- prebuild-rootsetup.sh
+++ prebuild-rootsetup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-REBUILD_CHECK=$(grep 'python27' ~/.bashrc | awk '{ print $1}')
-if [ -z "$REBUILD_CHECK// " ]; then
+PYTHON27_CHECK=$(grep 'python27' ~/.bashrc | awk '{ print $1}')
+if [ -z "${PYTHON27_CHECK// }" ]; then
   printf "Set up python2.7 for root user"
   sudo sed -i "\$a source /opt/rh/python27/enable" ~/.bashrc
 fi
